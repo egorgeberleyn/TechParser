4108bb802bde66558038de222026a76b2d902802
Egor Geberleyn <egeberleyn@gmail.com>
2022-06-16 22:39:20 +0300

    V4

--- Core/Data/DbInitializer.cs
+++ Core/Data/DbInitializer.cs
@@ -4,9 +4,9 @@ namespace TechParser.Core.Data
 {
     public class DbInitializer
     {
-        public static async Task Initialize(ParserDbContext context) //инициализация бд на основе контекста
+        public static void Initialize(ParserDbContext context) //инициализация бд на основе контекста
         {
-            //if (context.Database.EnsureCreated())//если бд не создана, создается новая
+            if (context.Database.EnsureCreated())//если бд не создана, создается новая
             {
                 var parser = new Parser.Parser(@"https://metalloobrabotchiki.ru");
                 await parser.ParseProviders(context);
